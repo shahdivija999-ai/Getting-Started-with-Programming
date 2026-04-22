69a222921b1a0d436702d91f119c00ed0421951b
shahdivija999-ai <shahdivija999@gmail.com>
2026-04-22 16:58:48 -0500

    V1

---
+++ Activity1.py
@@ -0,0 +1,14 @@
+#Print a sentence
+print ("Welcome to the world of programming")
+
+#Print a number
+print (4)
+
+#Next line after print
+print ("Hello Python \n")
+
+#Pass multiple arguments for printing
+print ("Hello" ,5)
+
+#End argument of Print statement
+print ("Welcome to" , end= "*")
